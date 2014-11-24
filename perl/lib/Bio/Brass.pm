1704a53ce9d7115cb46b3651f7a3f4b2f2cb5725
Keiran Raine <keiranmraine@gmail.com>
2014-11-24 15:39:36 +0000

    Improved memory foot print and prevent regions that overlap extreme depth from being assembled

--- perl/lib/Bio/Brass.pm
+++ perl/lib/Bio/Brass.pm
@@ -2,21 +2,21 @@ package Bio::Brass;
 
 ########## LICENCE ##########
 # Copyright (c) 2014 Genome Research Ltd.
-# 
+#
 # Author: Cancer Genome Project <cgpit@sanger.ac.uk>
-# 
+#
 # This file is part of BRASS.
-# 
+#
 # BRASS is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License as published by the Free
 # Software Foundation; either version 3 of the License, or (at your option) any
 # later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ########## LICENCE ##########
@@ -36,7 +36,7 @@ our @EXPORT = qw(find_breakpoints find_dusty_vertices
 		 is_dusty get_isolated_bp_alignment get_isolated_bp_surrounding_region
 		 $VERSION);
 
-our $VERSION = '2.0.0';
+our $VERSION = '2.0.1';
 
 =head1 NAME
 

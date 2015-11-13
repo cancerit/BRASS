d97eeef8fece281519fefb53881187225dd6f5c5
Keiran Raine <keiranmraine@gmail.com>
2015-11-13 20:30:19 +0000

    version bump

--- perl/lib/Bio/Brass.pm
+++ perl/lib/Bio/Brass.pm
@@ -46,7 +46,7 @@ our @EXPORT = qw(find_breakpoints find_dusty_vertices
 		 is_dusty get_isolated_bp_alignment get_isolated_bp_surrounding_region
 		 $VERSION);
 
-our $VERSION = '3.0.3';
+our $VERSION = '4.0.0';
 
 =head1 NAME
 

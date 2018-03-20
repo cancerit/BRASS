b0536805f5dcff3eb4405c508613023de1f04aa0
Keiran Raine <kr2@sanger.ac.uk>
2018-03-20 19:52:33 +0000

    Reduce disk I/O for trivial cpu

--- perl/bin/coverage_merge.pl
+++ perl/bin/coverage_merge.pl
@@ -39,8 +39,8 @@ use Try::Tiny qw(try catch finally);
 use Const::Fast qw(const);
 use List::Util qw(first);
 
-const my $FILE_PAIR => '%s.%s.ngscn.bed';
-const my $FILE_FOLD => '%s.%s.ngscn.fb_reads.bed';
+const my $FILE_PAIR => '%s.%s.ngscn.bed.gz';
+const my $FILE_FOLD => '%s.%s.ngscn.fb_reads.bed.gz';
 
 die "Usage: genome.fa.fai sample_name indir include_list" unless(scalar @ARGV >= 3);
 
@@ -90,7 +90,7 @@ sub cat_to_gzip {
     push @args, sprintf $format, $sample, $chr;
     die "Expected file missing $indir/$args[-1]\n" unless(-e $args[-1]);
   }
-  my $command = qq{bash -c 'set -o pipefail; cat @args | gzip -c > $outfile'};
+  my $command = qq{bash -c 'set -o pipefail; zcat @args | gzip -c > $outfile'};
   warn $command."\n";
   system($command) == 0 or die "Failed to merge files to $outfile: $!\n";
 }

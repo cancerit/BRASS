5ce48375559e38b2543c4229cbe089ed08333056
Keiran Raine <kr2@sanger.ac.uk>
2020-04-18 15:58:47 +0000

    Cleanup some issues

--- perl/bin/get_abs_bkpts_from_clipped_reads.pl
+++ perl/bin/get_abs_bkpts_from_clipped_reads.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 ########## LICENCE ##########
-# Copyright (c) 2014-2018 Genome Research Ltd.
+# Copyright (c) 2014-2020 Genome Research Ltd.
 #
 # Author: CASM/Cancer IT <cgphelp@sanger.ac.uk>
 #
